ef1f0c35ab44f01f22dc8d517023eb9a7c0bf003
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-19 12:02:51 +0200

    Migrate MainActivity to use navigation

---
+++ app/src/main/java/nat/loudj/duolingodictionary/HelloFragment.kt
@@ -0,0 +1,22 @@
+package nat.loudj.duolingodictionary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HelloFragment] factory method to
+ * create an instance of this fragment.
+ */
+class HelloFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hello, container, false)
+    }
+}
