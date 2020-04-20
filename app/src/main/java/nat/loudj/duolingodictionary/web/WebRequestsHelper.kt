d689e09750fbad6e5c8c33d5531053d72b2262f1
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-20 18:04:13 +0200

    Create WebRequestHelper

--- app/src/main/java/nat/loudj/duolingodictionary/web/WebRequestsHelper.kt
+++ app/src/main/java/nat/loudj/duolingodictionary/web/WebRequestsHelper.kt
@@ -1,5 +1,8 @@
 package nat.loudj.duolingodictionary.web
 
+import android.net.Uri
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -7,5 +10,22 @@ import okhttp3.Response
 object WebRequestsHelper {
     private val client = OkHttpClient.Builder().retryOnConnectionFailure(true).build()
 
-    fun execute(request: Request): Response = client.newCall(request).execute()
+    suspend fun execute(request: Request): Response = withContext(Dispatchers.IO) {
+        client.newCall(request).execute()
+    }
+
+    fun createRequest(baseUrl: String, path: String, vararg params: Pair<String, String>): Request {
+        val url = buildUrl(baseUrl, path, *params)
+        return Request.Builder().url(url).build()
+    }
+
+    private fun buildUrl(
+        baseUrl: String,
+        path: String,
+        vararg params: Pair<String, String>
+    ): String {
+        val completeUrl = Uri.Builder().scheme("https").authority(baseUrl).appendPath(path)
+        params.forEach { completeUrl.appendQueryParameter(it.first, it.second) }
+        return completeUrl.build().toString()
+    }
 }
